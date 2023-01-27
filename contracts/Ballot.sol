414db8d1e3cfb03eb8c4a9b4339d0fa1fc1b7ae1
Braun_Alex <lamanasakasa@gmail.com>
2023-01-27 18:01:15 +0200

    The first version of contract

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -10,33 +10,85 @@ contract Ballot {
     struct Poll {
         string title;
         string question;
-        string[] options;
+        string[] proposals;
         address author;
         uint256 whenCreated;
+        uint duration;
     }
     
     mapping(uint => Poll) pollPerId;
 
     mapping(address => uint[]) pollsPerAddress;
 
-    mapping(uint => mapping(string => uint)) votesPerPoll;
+    mapping(uint => uint[]) votesPerPoll;
+
+    mapping(uint => address[]) castedVotes;
 
     function createPoll (string memory _title, string memory _question, 
-    string[] memory _options) public {
+    string[] memory _proposals, uint _duration) public {
+        totalPolls += 1;
         address _author = msg.sender;
         uint _whenCreated = block.timestamp;
-        string memory _optionsData;
-        for (uint i = 0; i < _options.length; i++) {
-            string.concat(bytes(_optionsData), bytes(_options[i]));
+        string memory _proposalsData;
+        for (uint i = 0; i < _proposals.length; i++) {
+            string.concat(bytes(_proposalsData), bytes(_proposals[i]));
         }
         uint randomHash = uint(keccak256(abi.encodePacked(keccak256(abi.encodePacked(
             _title,
             _question,
-            _optionsData,
+            _proposalsData,
             _author,
-            _whenCreated
+            _whenCreated,
+            _duration
         )))));
-        pollPerId[randomHash] = Poll(_title, _question, _options, _author, _whenCreated);
+        pollPerId[randomHash] = Poll(
+            _title, 
+            _question, 
+            _proposals, 
+            _author, 
+            _whenCreated, 
+            _duration);
         pollsPerAddress[_author].push(randomHash);
+    }
+
+    function getInfo(uint _id) public view returns (Poll memory) {
+        return pollPerId[_id];
+    }
+
+    function getWinningProposal(uint _pollId) public view returns (string memory) {
+        Poll memory poll = pollPerId[_pollId];
+        require(!(block.timestamp >= poll.whenCreated + poll.duration), "Poll has not been finished!");
+        uint[] memory votes = votesPerPoll[_pollId];
+        uint winningProposalIndex = 0;
+        uint maxCount = 0;
+        for (uint i = 0; i < votes.length; i++) {
+            if (votes[i] > maxCount) {
+                maxCount = votes[i];
+                winningProposalIndex = i;
+            }
+        }
+        return pollPerId[_pollId].proposals[winningProposalIndex];
+    }
+
+    function vote(uint _pollId, string memory _pollOption) public {
+        Poll memory poll = pollPerId[_pollId];
+        require(block.timestamp >= poll.whenCreated + poll.duration, "Poll has been finished!");
+        address[] memory votes = castedVotes[_pollId];
+        for (uint i = 0; i < votes.length; i++) {
+            if (votes[i] == msg.sender) {
+                revert("Voter has already voted");
+            }
+        }
+        string[] memory proposals = poll.proposals;
+        bool doesExistProposal = false;
+        for (uint i = 0; i < proposals.length; i++) {
+            if (keccak256(abi.encodePacked(proposals[i])) == 
+            keccak256(abi.encodePacked(_pollOption))) {
+                doesExistProposal = true;
+                votesPerPoll[_pollId][i] += 1;
+            }
+        }
+        require(doesExistProposal, "Such proposal does not exist");
+        castedVotes[_pollId].push(msg.sender);
     }
 }
