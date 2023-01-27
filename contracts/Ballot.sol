c2cc582b35f916ec9cfee95a318b4d0df0c12edb
Braun_Alex <lamanasakasa@gmail.com>
2023-01-27 15:22:56 +0200

    Creating of createPoll() function

--- contracts/Ballot.sol
+++ contracts/Ballot.sol
@@ -5,22 +5,38 @@ pragma solidity >=0.7.0;
 contract Ballot {
     constructor() {}
 
-    uint256 totalBallots;
+    uint256 totalPolls;
 
     struct Poll {
         string title;
         string question;
         string[] options;
-        mapping(string => uint64) votes;
         address author;
         uint256 whenCreated;
     }
     
-    mapping(uint256 => Ballot) pollPerId;
+    mapping(uint => Poll) pollPerId;
 
-    mapping(address => uint256[]) pollsPerAddress;
+    mapping(address => uint[]) pollsPerAddress;
+
+    mapping(uint => mapping(string => uint)) votesPerPoll;
 
     function createPoll (string memory _title, string memory _question, 
     string[] memory _options) public {
+        address _author = msg.sender;
+        uint _whenCreated = block.timestamp;
+        string memory _optionsData;
+        for (uint i = 0; i < _options.length; i++) {
+            string.concat(bytes(_optionsData), bytes(_options[i]));
+        }
+        uint randomHash = uint(keccak256(abi.encodePacked(keccak256(abi.encodePacked(
+            _title,
+            _question,
+            _optionsData,
+            _author,
+            _whenCreated
+        )))));
+        pollPerId[randomHash] = Poll(_title, _question, _options, _author, _whenCreated);
+        pollsPerAddress[_author].push(randomHash);
     }
 }
