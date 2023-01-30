3e2ae7cfaf7ef87e89ed1c6df9eea65f3201335a
Braun_Alex <lamanasakasa@gmail.com>
2023-01-30 12:51:57 +0200

    Creating of the poll in the blockchain has been added

--- src/pages/CreatingOfPoll.vue
+++ src/pages/CreatingOfPoll.vue
@@ -66,7 +66,7 @@
         autogrow
         :rules="[
           val => val !== null && val !== '' || 'Поле тривалості опитування не може бути порожньою',
-          val => val > 0 || 'Поле тривалості опитування має бути додатнім цілим числом'
+          val => val > 0 || 'Поле тривалості опитування має бути додатнім числом'
         ]"
       />
 
@@ -83,17 +83,25 @@
 </template>
 
 <script setup lang="ts">
-import { ref, Ref, inject } from 'vue'
-import { useQuasar, format } from 'quasar'
+import { ref, Ref, inject, computed, ComputedRef } from 'vue'
+import { useQuasar, format, QSpinnerGears } from 'quasar'
+import { ethers } from 'ethers'
+import { abi } from 'src/Ballot.json'
 const $q = useQuasar()
-const title = ref(null)
-const question = ref(null)
+const title: Ref<null> = ref(null)
+const question: Ref<null> = ref(null)
 const proposals: Ref<string[] | null> = ref(null)
-const duration = ref(null)
+const duration: Ref<null> = ref(null)
+const durationInSeconds: ComputedRef<number | null> = computed(() => {
+  return duration.value === null ? null : 3600 * duration.value
+})
 const accept = ref(false)
 const walletConnected = ref(false)
+const contractAddress = process.env.VUE_APP_BALLOT_CONTRACT
+const contractMutated = ref(false)
 
 inject('walletConnected', walletConnected)
+inject('contractMutated', contractMutated)
 
 function onSubmit () {
   if (accept.value !== true) {
@@ -111,12 +119,74 @@ function onSubmit () {
       message: 'Вам потрібно під\'єднати гаманець MetaMask для створення голосування он-чейн'
     })
   } else {
-    $q.notify({
-      color: 'green-3',
-      textColor: 'white',
-      icon: 'cloud_done',
-      message: 'Відправлено'
-    })
+    // @ts-expect-error Window.ethers not TS
+    if (typeof window.ethereum !== 'undefined') {
+      // @ts-expect-error Window.ethers not TS
+      const provider = new ethers.providers.Web3Provider(window.ethereum)
+      const signer = provider.getSigner()
+      const contract = new ethers.Contract(
+        contractAddress as string,
+        abi,
+        provider
+      )
+      $q.loading.show({
+        spinner: QSpinnerGears,
+        message: 'Підписання транзакції створення голосування...'
+      })
+      try {
+        contract.create(
+          title.value,
+          question.value,
+          proposals.value,
+          durationInSeconds.value, {
+            gasLimit: 300000
+          }).then((transaction: any) => {
+          $q.loading.hide()
+          $q.loading.show({
+            spinner: QSpinnerGears,
+            message: 'Обробка транзакції валідаторами і її включення у блок блокчейну...'
+          })
+          transaction.wait().then(() => {
+            $q.notify({
+              color: 'green-3',
+              textColor: 'white',
+              icon: 'cloud_done',
+              message: 'Голосування було успішно створено он-чейн'
+            })
+            contractMutated.value = true
+            contractMutated.value = false
+          }).catch(() => {
+            $q.notify({
+              color: 'red-5',
+              textColor: 'white',
+              icon: 'warning',
+              message: 'Обробку транзакції було відхилено валідаторами. Спробуйте ще раз'
+            })
+          })
+        }).catch(() => {
+          $q.notify({
+            color: 'red-5',
+            textColor: 'white',
+            icon: 'warning',
+            message: 'Підписання транзакції було відхилено'
+          })
+        }).finally(() => {
+          $q.loading.hide()
+        })
+      } catch (error: any) {
+        $q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: error.message
+        })
+      }
+    } else {
+      $q.notify({
+        type: 'negative',
+        message: 'MetaMask не встановлено'
+      })
+    }
   }
 }
 
@@ -124,6 +194,7 @@ function onReset () {
   title.value = null
   question.value = null
   proposals.value = null
+  duration.value = null
   accept.value = false
 }
 
