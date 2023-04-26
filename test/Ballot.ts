50c54cad8f0c1b09adc52914d11aad3f37233408
Braun_Alex <lamanasakasa@gmail.com>
2023-04-27 00:55:19 +0300

    Reverting unit test has been added

--- test/Ballot.ts
+++ test/Ballot.ts
@@ -1,4 +1,5 @@
 import "hardhat";
+import "@nomicfoundation/hardhat-toolbox";
 import "@nomiclabs/hardhat-ethers";
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
@@ -17,6 +18,12 @@ describe("Ballot", function() {
     it("Contract does not have polls", async function() {
       const { ballot } = await loadFixture(deployContractFixture);
       expect(await ballot.getAllPolls()).to.be.empty;
+    })
+    it("Contract with no polls reverts getting results", async function() {
+      const { ballot } = await loadFixture(deployContractFixture);
+      await expect(ballot.getResults(0)).to.be.revertedWithPanic();
+      await expect(ballot.getResults(3)).to.be.revertedWithPanic();
+      await expect(ballot.getResults(333)).to.be.revertedWithPanic();
     })
   })
 })
